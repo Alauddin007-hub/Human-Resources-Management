fc802e6473c35e03b9693af8e8671a436a4cdaef
Alauddin007-hub <mdalauddinalsafin@gmail.com>
2023-10-01 10:09:05 +0600

    fshd

--- ATM-Project/Database/hrm_project(3).sql
+++ ATM-Project/Database/hrm_project(3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 10, 2023 at 11:43 AM
+-- Generation Time: Oct 01, 2023 at 06:07 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -30,15 +30,22 @@ SET time_zone = "+00:00";
 CREATE TABLE `attendances` (
   `id` int(11) NOT NULL,
   `employee_id` int(11) NOT NULL,
-  `date` date NOT NULL,
-  `clock_in` time NOT NULL,
-  `clock_out` time NOT NULL,
-  `total_work` float NOT NULL,
-  `status` int(11) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp(),
+  `time_in` time NOT NULL,
+  `time_out` time NOT NULL,
+  `total_work_hour` double NOT NULL,
+  `status` int(1) NOT NULL,
   `created_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `modified_at` timestamp NULL DEFAULT '0000-00-00 00:00:00',
+  `modified_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `attendances`
+--
+
+INSERT INTO `attendances` (`id`, `employee_id`, `date`, `time_in`, `time_out`, `total_work_hour`, `status`, `created_at`, `modified_at`, `deleted_at`) VALUES
+(1, 1, '2023-09-11', '09:11:40', '00:00:00', 0, 1, '2023-09-11 03:11:40', '0000-00-00 00:00:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -51,9 +58,9 @@ CREATE TABLE `departments` (
   `name` varchar(30) NOT NULL,
   `status` int(2) NOT NULL DEFAULT 1,
   `create_by` varchar(30) DEFAULT NULL,
-  `create_date` timestamp NULL DEFAULT NULL,
+  `create_date` timestamp NULL DEFAULT current_timestamp(),
   `modified_by` varchar(30) DEFAULT NULL,
-  `modified_date` timestamp NULL DEFAULT NULL
+  `modified_date` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=COMPACT;
 
 --
@@ -62,7 +69,11 @@ CREATE TABLE `departments` (
 
 INSERT INTO `departments` (`id`, `name`, `status`, `create_by`, `create_date`, `modified_by`, `modified_date`) VALUES
 (1, 'IT', 1, 'Admin', '2017-05-24 06:37:11', '', '2017-05-05 18:00:00'),
-(2, 'Coaching', 1, 'Admin', '2017-05-24 06:37:29', '', '0000-00-00 00:00:00');
+(2, 'Training', 1, 'Admin', '2017-05-24 06:37:29', '', '0000-00-00 00:00:00'),
+(3, 'production', 1, '1', '2023-09-16 18:00:00', '1', '2023-09-16 18:00:00'),
+(4, 'management', 1, '1', '2023-09-16 18:00:00', '1', '2023-09-16 18:00:00'),
+(5, 'finance', 1, '1', '2023-09-17 03:12:20', '1', '2023-09-17 03:12:20'),
+(6, 'Marketing', 1, '1', '2023-09-17 03:12:20', '1', '2023-09-17 03:12:20');
 
 -- --------------------------------------------------------
 
@@ -72,7 +83,7 @@ INSERT INTO `departments` (`id`, `name`, `status`, `create_by`, `create_date`, `
 
 CREATE TABLE `employee_designations` (
   `id` int(11) NOT NULL,
-  `title` varchar(30) NOT NULL,
+  `position` varchar(30) NOT NULL,
   `status` int(2) NOT NULL DEFAULT 1,
   `created_by` varchar(30) DEFAULT NULL,
   `create_date` timestamp NULL DEFAULT NULL,
@@ -84,7 +95,7 @@ CREATE TABLE `employee_designations` (
 -- Dumping data for table `employee_designations`
 --
 
-INSERT INTO `employee_designations` (`id`, `title`, `status`, `created_by`, `create_date`, `modified_by`, `modified_date`) VALUES
+INSERT INTO `employee_designations` (`id`, `position`, `status`, `created_by`, `create_date`, `modified_by`, `modified_date`) VALUES
 (1, 'Teacher', 1, 'Admin', '2017-05-24 06:36:17', '', '2017-05-24 06:36:17'),
 (2, 'Programmer', 1, 'Admin', '2017-05-03 18:00:00', NULL, NULL);
 
@@ -96,24 +107,37 @@ INSERT INTO `employee_designations` (`id`, `title`, `status`, `created_by`, `cre
 
 CREATE TABLE `employee_info` (
   `Id` int(11) NOT NULL,
+  `emp_id` varchar(15) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `First_name` varchar(30) NOT NULL,
   `Last_name` varchar(30) NOT NULL,
   `Phone` varchar(20) NOT NULL,
   `Email` varchar(30) NOT NULL,
   `Age` int(11) NOT NULL,
-  `Startdate` date NOT NULL DEFAULT current_timestamp(),
+  `Joining_date` date NOT NULL DEFAULT current_timestamp(),
   `Education_exp` varchar(100) NOT NULL,
   `Add_exp` varchar(100) NOT NULL,
   `department_id` int(11) NOT NULL,
   `designation_id` int(11) NOT NULL,
+  `schedules_id` int(11) NOT NULL,
   `Bank_name` varchar(40) NOT NULL,
   `Bank_account_number` mediumint(40) NOT NULL,
   `Salary` int(11) NOT NULL,
+  `photo` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `created_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `create_by` varchar(30) NOT NULL,
   `modified_by` varchar(30) NOT NULL,
   `modified_date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employee_info`
+--
+
+INSERT INTO `employee_info` (`Id`, `emp_id`, `First_name`, `Last_name`, `Phone`, `Email`, `Age`, `Joining_date`, `Education_exp`, `Add_exp`, `department_id`, `designation_id`, `schedules_id`, `Bank_name`, `Bank_account_number`, `Salary`, `photo`, `created_date`, `create_by`, `modified_by`, `modified_date`) VALUES
+(1, 'emp101', 'Mr.', 'Y', '01874563252', 'y@gmail.com', 25, '2023-09-11', 'BBA(Hon\'s)in Accounting', 'Marketing Oficer & Wed devoloper', 2, 1, 3, 'AB Bank Ltd.', 7896354, 3500, '', '2023-09-11 03:07:17', '1', '', '2023-09-11 03:07:17'),
+(2, 'emp102', 'MD', 'Alauddin', '01874563252', 'a@gmail.com', 26, '2023-09-16', 'Masters in Acoounting', '2 Years in Sales Marketing ', 1, 2, 4, 'Estarn Bank Ltd.', 6945634, 35000, '', '2023-09-16 03:08:15', '', '', '2023-09-16 03:08:15'),
+(10, 'emp103', 'Jhon', 'Deo', '448562138', 'abc@gmail.com', 28, '2023-09-27', 'MBA(HR Management)', 'mokam', 0, 0, 2, 'dutch bangla bank', 4758623, 0, '', '2023-09-27 07:05:56', '', '', '2023-09-27 07:05:56'),
+(11, 'emp104', 'Jhon', 'Deo', '448562138', 'abc@gmail.com', 28, '2023-09-27', 'MBA(HR Management)', 'mokam', 0, 0, 3, 'dutch bangla bank', 4758623, 0, '', '2023-09-27 07:28:50', '', '', '2023-09-27 07:28:50');
 
 -- --------------------------------------------------------
 
@@ -260,7 +284,32 @@ INSERT INTO `notices` (`id`, `subject`, `notice_date`, `notice_time`, `descripti
 (6, 'Board Mitting', '2017-05-22', '10:01:00', 'fh htyt rtyt t rt', 1, 'admin', '2017-05-21 20:00:50', '', NULL),
 (7, '', '0000-00-00', '00:00:00', '', 1, 'admin', '2017-05-23 14:57:17', '', NULL),
 (8, 'Traning', '2023-09-15', '22:15:00', 'Project Discation', 1, 'admin', '2023-08-22 05:48:19', '', NULL),
-(9, 'Dhoira pida', '2023-08-22', '13:00:00', 'Jotokhon para jay', 1, 'admin', '2023-08-22 06:45:01', '', NULL);
+(9, 'Dhoira pida', '2023-08-22', '13:00:00', 'Jotokhon para jay', 1, 'admin', '2023-08-22 06:45:01', '', NULL),
+(10, '', '0000-00-00', '00:00:00', '', 1, '', NULL, '', NULL),
+(11, '', '0000-00-00', '00:00:00', '', 1, '', NULL, '', NULL),
+(12, '', '0000-00-00', '00:00:00', '', 1, '', NULL, '', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedules`
+--
+
+CREATE TABLE `schedules` (
+  `id` int(11) NOT NULL,
+  `time_in` time NOT NULL,
+  `time_out` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `schedules`
+--
+
+INSERT INTO `schedules` (`id`, `time_in`, `time_out`) VALUES
+(1, '07:00:00', '16:00:00'),
+(2, '08:00:00', '17:00:00'),
+(3, '09:00:00', '18:00:00'),
+(4, '10:00:00', '19:00:00');
 
 -- --------------------------------------------------------
 
@@ -308,7 +357,9 @@ ALTER TABLE `employee_designations`
 -- Indexes for table `employee_info`
 --
 ALTER TABLE `employee_info`
-  ADD PRIMARY KEY (`Id`);
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `fk_employee_department` (`department_id`),
+  ADD KEY `fk_employee_designation` (`designation_id`);
 
 --
 -- Indexes for table `employee_leavs`
@@ -341,6 +392,12 @@ ALTER TABLE `notices`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `schedules`
+--
+ALTER TABLE `schedules`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -355,13 +412,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `attendances`
 --
 ALTER TABLE `attendances`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `departments`
 --
 ALTER TABLE `departments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `employee_designations`
@@ -373,7 +430,7 @@ ALTER TABLE `employee_designations`
 -- AUTO_INCREMENT for table `employee_info`
 --
 ALTER TABLE `employee_info`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `employee_leavs`
@@ -403,7 +460,13 @@ ALTER TABLE `live_events`
 -- AUTO_INCREMENT for table `notices`
 --
 ALTER TABLE `notices`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `schedules`
+--
+ALTER TABLE `schedules`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
